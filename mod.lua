77333589b105091acae7a2cb3b06870b410de8e3
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-06-13 17:26:47 -0700

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -1,5 +1,37 @@
-ModCheck = {
-    [2681346262] = true,
+ModIDS = {
+	2681346262,
 }
 
-return ModCheck
+Owner = {
+	1,
+}
+
+--\\ Don't mess with anything below this
+function Identify()
+	for _,Player in pairs(game:GetService('Players'):GetChildren()) do
+		if table.find(ModIDS, Player.UserId) then
+			if Player.Character then
+				if Player.Character.Parent.Name == 'Characters' then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+				end
+			end
+		elseif
+			table.find(Owner, Player.UserId) then
+			if Player.Character then
+				if Player.Character.Parent.Name == 'Characters' then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. Player.DisplayName)
+				end
+			end
+		if
+			Player.Character then
+			if Player.Character.Parent.Name == 'Characters' then
+				if not Player.Character.RightUpperLeg:FindFirstChild('OriginalSize') then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🌌]' .. Player.DisplayName)
+				end
+			end
+		end
+	end
+	end
+	end
+local success,err = pcall(Identify)
+return ModIDS
